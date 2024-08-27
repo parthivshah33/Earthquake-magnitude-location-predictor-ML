79878a1f239eedd60bf50870de08ff133cb762da
Parthiv Shah <84636508+parthivshah33@users.noreply.github.com>
2024-08-27 23:36:28 +0530

    commented "app.run()" due to flask bug

--- app.py
+++ app.py
@@ -70,5 +70,5 @@ def location():
 def future_eq():
     return render_template("future_eq.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
